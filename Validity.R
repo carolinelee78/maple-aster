3f9e63500bca31f807ce2b0ec4669247041da99e
Caroline Lee <carolinelee@Carolines-Laptop-2.local>
2025-04-14 12:08:23 -0500

    update

---
+++ Validity.R
@@ -0,0 +1,136 @@
+MEAQ62_FacetConvergence <- c(.67, .49, .51, .42, .36, 
+                             .38, .55, .39, .23,
+                             .25, .48, .39,
+                             .26, .05,
+                             .25)
+
+mean(MEAQ62_FacetConvergence)
+
+MEAQ62_FacetConvergence_BA <- c(.67, .49, .51, .42, .36)
+
+mean(MEAQ62_FacetConvergence_BA)
+
+MEAQ62_FacetConvergence_DA <- c(.67, .38, .55, .39, .23)
+
+mean(MEAQ62_FacetConvergence_DA)
+
+MEAQ62_FacetConvergence_PR <- c(.49, .38, .25, .48, .39)
+
+mean(MEAQ62_FacetConvergence_PR)
+
+MEAQ62_FacetConvergence_DS <- c(.51, .55, .25, .26, .05)
+
+mean(MEAQ62_FacetConvergence_DS)
+
+MEAQ62_FacetConvergence_RD <- c(.42, .39, .48, .26, .25)
+
+mean(MEAQ62_FacetConvergence_RD)
+
+MEAQ62_FacetConvergence_DE <- c(.36, .23, .39, .05, .25)
+
+mean(MEAQ62_FacetConvergence_DE)
+
+MEAQGA_FacetConvergence <- c(.59, .44, .45, .37, .3,
+                             .39, .50, .38, .24,
+                             .22, .44, .46,
+                             .21, .07,
+                             .27)
+
+mean(MEAQGA_FacetConvergence)
+
+MEAQGA_FacetConvergence_BA <- c(.59, .44, .45, .37, .3)
+
+mean(MEAQGA_FacetConvergence_BA)
+
+MEAQGA_FacetConvergence_DA <- c(.59, .39, .50, .38, .24)
+
+mean(MEAQGA_FacetConvergence_DA)
+
+MEAQGA_FacetConvergence_PR <- c(.44, .39, .22, .44, .46)
+
+mean(MEAQGA_FacetConvergence_PR)
+
+MEAQGA_FacetConvergence_DS <- c(.45, .50, .22, .21, .07)
+
+mean(MEAQGA_FacetConvergence_DS)
+
+MEAQGA_FacetConvergence_RD <- c(.37, .38, .44, .21, .27)
+
+mean(MEAQGA_FacetConvergence_RD)
+
+MEAQGA_FacetConvergence_DE <- c(.30, .24, .46, .07, .27)
+
+mean(MEAQGA_FacetConvergence_DE)
+
+MEAQSF_FacetConvergence <- c(.57, .47, .40, .42, .32,
+                             .30, .51, .31, .12,
+                             .20, .48, .39, 
+                             .17, .09,
+                             .39)
+
+mean(MEAQSF_FacetConvergence)
+
+MEAQSF_FacetConvergence_BA <- c(.57, .47, .40, .42, .32)
+
+mean(MEAQSF_FacetConvergence_BA)
+
+MEAQSF_FacetConvergence_DA <- c(.57, .30, .51, .31, .12)
+
+mean(MEAQSF_FacetConvergence_DA)
+
+MEAQSF_FacetConvergence_PR <- c(.47, .30, .20, .48, .39)
+
+mean(MEAQSF_FacetConvergence_PR)
+
+MEAQSF_FacetConvergence_DS <- c(.40, .51, .20, .17, .09)
+
+mean(MEAQSF_FacetConvergence_DS)
+
+MEAQSF_FacetConvergence_RD <- c(.42, .31, .48, .17, .39)
+
+mean(MEAQSF_FacetConvergence_RD)
+
+MEAQSF_FacetConvergence_DE <- c(.32, .12, .39, .09, .39)
+
+mean(MEAQSF_FacetConvergence_DE)
+
+MEAQBF_FacetConvergence <- c(.39, .40, .32, .30, .21,
+                             .20, .23, .25, .20,
+                             .25, .41, .26,
+                             .15, .09, 
+                             .22)
+
+mean(MEAQBF_FacetConvergence)
+
+MEAQBF_FacetConvergence_BA <- c(.39, .40, .32, .30, .21)
+
+mean(MEAQBF_FacetConvergence_BA)
+
+MEAQBF_FacetConvergence_DA <- c(.39, .20, .23, .25, .20)
+
+mean(MEAQBF_FacetConvergence_DA)
+
+MEAQBF_FacetConvergence_PR <- c(.40, .20, .25, .41, .26)
+
+mean(MEAQBF_FacetConvergence_PR)
+
+MEAQBF_FacetConvergence_DS <- c(.32, .23, .25, .15, .09)
+
+mean(MEAQBF_FacetConvergence_DS)
+
+MEAQBF_FacetConvergence_RD <- c(.30, .25, .41, .15, .22)
+
+mean(MEAQBF_FacetConvergence_RD)
+
+MEAQBF_FacetConvergence_DE <- c(.21, .20, .26, .09, .22)
+
+mean(MEAQBF_FacetConvergence_DE)
+
+
+# ================================================================
+
+(.53 + .55 + .52 + .51 + .46)/5
+
+(.43 + .41 + .34 + .37 + .41)/5
+
+(.55 + .56 + .40 + .44 + .50)/5
